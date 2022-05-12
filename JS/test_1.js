d15519102039c6485cd86428a182a5ce47699acd
Nikita Lukashevich <nick939317@gmail.com>
2022-05-12 15:49:39 +0300

    operator ==

---
+++ JS/test_1.js
@@ -0,0 +1,9 @@
+// Оператор сравнения ==
+function testEqual(val) {
+    if (val == 12) { //если val будет равен 12, значит значит условие истино, и функция вернет Equal 
+      return "Equal";
+    }
+    return "Not Equal"; // если услловия не выполняеться функция вернет Not Equal
+  }
+  
+  testEqual(10);
